d044f08d8db339d55069b03eacf6376eef50e374
Hüseyin YİĞİT <yigit.hsyn@gmail.com>
2024-10-31 17:56:59 +0300

    package initilized

---
+++ setup.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import pathlib
+from setuptools import setup, find_packages, find_namespace_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# Version: major.minor.patch
+VERSION = (HERE / "VERSION").read_text().strip()
+
+# License
+LICENSE = 'The Unlicense'
+
+# Required Python version
+PYTHON_VERSION = '>= 3'
+
+
+setup \
+    ( name             = "adf"
+    , version          = VERSION
+    , description      = "Antenna Measurement Software (AMS) File Format (ADF) Reader "
+    , long_description = README
+    , long_description_content_type='text/markdown'
+    , license          = LICENSE
+    , author           = "Hüseyin YİĞİT"
+    , author_email     = "yigit.hsyn@gmail.com"
+    , install_requires = \
+        [ 'h5py>=3.11.0' ]
+    , packages         = find_namespace_packages(where='src'),
+      package_dir      = {"": "src"}
+    , platforms        = 'Any'
+    , url              = "https://github.com/atamlab/ams-adf"
+    , python_requires  = PYTHON_VERSION
+    , classifiers      = \
+        [ 'Development Status :: 2 - Pre-Alpha'
+        , 'License :: OSI Approved :: ' + LICENSE
+        , 'Operating System :: OS Independent'
+        , 'Programming Language :: Python'
+        , 'Programming Language :: Python :: 3'
+        , 'Intended Audience :: Science/Research'
+        , 'Intended Audience :: Other Audience'
+        , 'Topic :: Scientific/Engineering'
+        , 'Topic :: Scientific/Engineering :: Mathematics'
+        , 'Topic :: Scientific/Engineering :: Physics'
+        ]
+    )
---
+++ src/adf/__init__.py
@@ -0,0 +1,1 @@
+from .measurementStart import measurementStart
